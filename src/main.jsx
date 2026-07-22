8f775507e7760233dbb3d75667cc13cca6bf47f2
juceiba22 <juceiba22@gmail.com>
2026-07-22 13:28:39 -0300

    a veeeeeeeeeeeeeeeeeeeeer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppShell from './AppShell'; // Tu archivo original intacto
 import './index.css'; // Tus estilos globales de Tailwind
+import './tabar-responsive.css'; // Estilos responsivos del diseño del panel de control
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
